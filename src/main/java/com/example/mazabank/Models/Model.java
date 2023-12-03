c68e8e84b52218b5497468d6de3e7fcafea41fda
Administrator <levantrungtinh13@gmail.com>
2023-12-04 06:25:18 +0700

    12/4

--- src/main/java/com/example/mazabank/Models/Model.java
+++ src/main/java/com/example/mazabank/Models/Model.java
@@ -5,8 +5,22 @@ import com.example.mazabank.Views.ViewFactory;
 public class Model {
     private static Model model;
     private final ViewFactory viewFactory;
+    private final DatabaseDriver databaseDriver;
+//    client data section
+    private Client client;
+    private  boolean clientLoginSuccessFlag;
+
+//    admin data section
+
+
     private Model() {
         this.viewFactory = new ViewFactory();
+        this.databaseDriver = new DatabaseDriver();
+        //    client data section
+        this.clientLoginSuccessFlag = false;
+        this.client = new Client("", "", "", null, null, null);
+        //    admin data section
+
     }
     public static synchronized Model getInstance() {
         if (model == null) {
@@ -18,6 +32,17 @@ public class Model {
     public ViewFactory getViewFactory() {
         return viewFactory;
     }
+    public DatabaseDriver getDatabaseDriver() {return databaseDriver; }
+
+    /*
+    * Client Method Section
+    * */
+    public boolean getClientLoginSuccessFlag() {return this.clientLoginSuccessFlag; }
+    public void setClientLoginSuccessFlag(boolean flag) {this.clientLoginSuccessFlag = flag; }
 
 
+
+    /*
+     * Admin Method Section
+     * */
 }
